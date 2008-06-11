94d6c68566911c3e5306fe6a25aa1955ef9a2db4
Jonas Smedegaard <dr@jones.dk>
2008-06-11 03:59:20 +0200

    Fix python-sugar.mk to work with recent python-sugar-toolkit where MANIFEST files are ignored and an internal hardcoded exception list is used instead.  Tighten build-dependency.

--- debian/cdbs/1/class/python-sugar.mk
+++ debian/cdbs/1/class/python-sugar.mk
@@ -30,7 +30,7 @@ include debian/cdbs/1/class/python-vars.mk
 include $(_cdbs_rules_path)/debhelper.mk$(_cdbs_makefile_suffix)
 
 # Declare Build-Deps for packages using this file
-CDBS_BUILD_DEPENDS := $(CDBS_BUILD_DEPENDS), python-sugar, python-sugar-toolkit (>= 0.81.4), unzip
+CDBS_BUILD_DEPENDS := $(CDBS_BUILD_DEPENDS), python-sugar, python-sugar-toolkit (>= 0.81.4-2~), unzip
 # FIXME: Resolve DEB_PYTHON_PACKAGES in build targets only
 ifeq (,$(cdbs_python_pkg_check)$(DEB_PYTHON_ARCH_PACKAGES))
   ifneq (, $(cdbs_python_compile_version))
@@ -49,29 +49,11 @@ endif
 
 DEB_PYTHON_SUGAR_PACKAGES = $(filter sugar-%-activity, $(DEB_PACKAGES))
 
-# Files or `find -name`-compatible search patterns to add to temp MANIFEST
-#DEB_PYTHON_SUGAR_MANIFEST = *.py *.po *.pot *.svg
-
-DEB_PYTHON_SUGAR_MANIFEST_REGEX =
-DEB_PYTHON_SUGAR_MANIFEST_IGNORE_REGEX = ^(\..*|.*/\..*|debian/.*)
-
 # TODO: Move this to buildvars.mk
 cdbs_pkgsrcdir = $(if $(DEB_PKGSRCDIR_$(cdbs_curpkg)),$(DEB_PKGSRCDIR_$(cdbs_curpkg)),$(DEB_SRCDIR))
 
-cdbs_python_sugar_manifest_files_tmp = $(patsubst %,-or -name '%',$(DEB_PYTHON_SUGAR_MANIFEST))
-cdbs_python_sugar_manifest_files = $(wordlist 2, $(words $(cdbs_python_sugar_manifest_files_tmp)), $(cdbs_python_sugar_manifest_files_tmp))
-
-cdbs_python_sugar_automanifest = $(if $(DEB_PYTHON_SUGAR_MANIFEST_$(cdbs_curpkg)),$(DEB_PYTHON_SUGAR_MANIFEST_$(cdbs_curpkg)),$(DEB_PYTHON_SUGAR_MANIFEST))
-
 pre-build::
 	mkdir -p debian/stamps-configure
-
-# FIXME: make these targets *not* run twice but respect stamps!
-$(patsubst %,configure/%,$(DEB_PYTHON_SUGAR_PACKAGES)) :: configure/%: debian/stamps-configure/%
-$(patsubst %,debian/stamps-configure/%,$(DEB_PYTHON_SUGAR_PACKAGES)) :: debian/stamps-configure/%:
-	$(if $(cdbs_python_sugar_automanifest),i="$(cdbs_pkgsrcdir)/MANIFEST"; if [ -e "$$i" ] && [ ! -e "$$i.cdbs-orig" ]; then mv "$$i" "$$i.cdbs-orig"; fi)
-	$(if $(cdbs_python_sugar_automanifest),cd "$(cdbs_pkgsrcdir)" && find * -type f $(if $(cdbs_python_sugar_manifest_files)$(DEB_PYTHON_SUGAR_MANIFEST_REGEX),'(' $(cdbs_python_sugar_manifest_files) $(if $(DEB_PYTHON_SUGAR_MANIFEST_REGEX),$(if $(cdbs_python_sugar_manifest_files),-or )-regex '$(DEB_PYTHON_SUGAR_MANIFEST_REGEX)') ')') $(if $(DEB_PYTHON_SUGAR_MANIFEST_IGNORE_REGEX),-not -regex '$(DEB_PYTHON_SUGAR_MANIFEST_IGNORE_REGEX))' > "$(cdbs_pkgsrcdir)/MANIFEST")
-	touch $@
 
 $(patsubst %,build/%,$(DEB_PYTHON_SUGAR_PACKAGES)) :: build/%:
 	for pythonver in $(cdbs_python_build_versions); do \
@@ -95,19 +77,15 @@ clean:: $(patsubst %,cleanpythonsugar/%,$(DEB_PYTHON_SUGAR_PACKAGES))
 ifeq (, $(cdbs_selected_pycompat))
 	echo "$(cdbs_pycompat)" >debian/pycompat
 endif # use pycompat
-	rm -rf debian/stamps-configure
 
 $(patsubst %,cleanpythonsugar/%,$(DEB_PYTHON_SUGAR_PACKAGES)) :: cleanpythonsugar/% : 
-	for pythonver in $(cdbs_python_build_versions); do \
-		/usr/bin/python$$ver $(cdbs_pkgsrcdir)/setup.py clean; \
-	done
-	find "$(cdbs_pkgsrcdir)" -maxdepth 1 -type f -name '*.xo' -exec rm -f '{}' ';'
+	-find "$(cdbs_pkgsrcdir)/dist" -maxdepth 1 -type f -name '*.xo' -exec rm -f '{}' ';'
+	-rmdir --ignore-fail-on-non-empty "$(cdbs_pkgsrcdir)/dist"
 	-IFS="`printf '\n'`" find "$(cdbs_pkgsrcdir)/locale" -type f \( -name '*.mo' -or -name 'activity.linfo' \) | while read path; do \
 		rm -f "$$path"; \
 		rmdir --ignore-fail-on-non-empty "`dirname "$$path"`"; \
 	done
 	-rmdir --ignore-fail-on-non-empty "$(cdbs_pkgsrcdir)/locale"
-	$(if $(cdbs_python_sugar_automanifest),i="$(cdbs_pkgsrcdir)/MANIFEST"; if test -e "$$i.cdbs-orig"; then mv -f "$$i.cdbs-orig" "$$i"; fi)
 
 ## TODO: Drop this when DEB_PYTHON_PACKAGES is only resolved in build targets
 pre-build clean::
